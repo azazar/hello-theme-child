d34bef0262b977b48d744198c8349908173a0ac0
Mikhail Yevchenko <m..@uo1.net>
2023-01-09 22:32:40 +0200

    Add freezeframe.js to scripts

--- functions.php
+++ functions.php
@@ -19,5 +19,7 @@ function hello_elementor_child_enqueue_scripts() {
 		],
 		'1.0.0'
 	);
+
+	wp_enqueue_script('custom', 'https://unpkg.com/freezeframe/dist/freezeframe.min.js');
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
