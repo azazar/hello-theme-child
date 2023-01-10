3152407fe67b1f1038d4c20bbed2218b186c2c86
Mikhail Yevchenko <m..@uo1.net>
2023-01-10 19:14:40 +0200

    Fix script names

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ function hello_elementor_child_enqueue_scripts() {
 		'1.0.0'
 	);
 
-	wp_enqueue_script('custom', 'https://unpkg.com/freezeframe/dist/freezeframe.min.js');
-	wp_enqueue_script('custom', get_stylesheet_directory_uri() . '/ff_init.js');
+	wp_enqueue_script('freezeframe', 'https://unpkg.com/freezeframe/dist/freezeframe.min.js');
+	wp_enqueue_script('freezeframe_init', get_stylesheet_directory_uri() . '/ff_init.js');
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
